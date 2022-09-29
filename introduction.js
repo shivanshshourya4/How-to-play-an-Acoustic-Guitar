fd0ae5a55934f5b64bf7934ad53b1144640a5246
shivanshshourya4 <92980002+shivanshshourya4@users.noreply.github.com>
2022-09-29 14:22:11 +0530

    Add files via upload

---
+++ introduction.js
@@ -0,0 +1,9 @@
+function main_p()
+{
+    window.location
+}
+
+function start_l()
+{
+    window.location.href="http://127.0.0.1:5501/webpages/learn/learn.html";
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+function In_t() 
+{
+ window.location.href="http://127.0.0.1:5501/webpages/introduction/introduction.html";
+}
+function start_l()
+{
+    window.location.href="http://127.0.0.1:5501/webpages/learn/learn.html";
+}
